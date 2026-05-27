2df1aec758753cc2b135cc1e014bb2edb546a524
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-05-27 13:41:41 +0100

    string methods

---
+++ week1-js/stringMethod.js
@@ -0,0 +1,13 @@
+const email = " DevTony779@gmail.com "
+ console.log(email.toLowerCase());
+ console.log(email.trim());
+ console.log(email.toUpperCase());
+
+ const fullName = "ajibo chibuzor anthony"
+ console.log(fullName.split(" ")); // turns the word or statement into area
+ console.log(fullName.replace("ajibo", "Ajibo")); // to replace a string
+ console.log(fullName.includes("Anthony")); // to check it it contains word or letter
+ console.log(fullName.indexOf("anthony")); // to return the index of a word or letter
+ console.log(fullName.match("chibuzor")); //  to search and return if found
+ console.log(fullName.endsWith("y"));  // to check for the last letter in sentence or word
+ 
