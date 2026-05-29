ef780a8a115216e7d96f4477a06ec29e7514ec39
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-05-29 07:22:03 +0100

    var lesson

---
+++ week1-js/variableDataTypes.js
@@ -0,0 +1,21 @@
+// var is no longer in use
+const fname=  "chibuzor";   // use quotes strings
+const age = 28   // without quotes for numbers 
+const isEmployed = false  // boolean  is true or false
+const lastName = "Tony"
+let username;
+let bvn
+let NIN
+let phone
+let profileImgURL
+let address;     // declared but never but values not assigned NOTE : const is not allowed for this type
+const  id = Symbol("id")    //symbol - unique identifier, rarely used
+ // const holds both int,string double, float, boolean etc  we use typeof to know the data type 
+ const money = 200.0
+ const int = 500
+ const negative = -100
+console.log(typeof age)
+console.log(typeof fname);
+console.log(typeof id);
+console.log(typeof money);
+
