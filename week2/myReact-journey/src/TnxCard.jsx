85d49c0b81fefe80874b4eb45ad64294d63ba8f8
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-06-09 14:03:46 +0100

    updated

--- week2/myReact-journey/src/TnxCard.jsx
+++ week2/myReact-journey/src/TnxCard.jsx
@@ -4,7 +4,6 @@ const TnxCard = ({type, amount,description, date}) => {
 
     return (
         <div>
-            //tenary styling is cool
             <div className={`p-4 mb-3 pl-5  ml-10   rounded-lg bg-grey-50 border-l-4 ${isCredit ? "border-green-500" : "border-red-500"}`}>
                 <h1> Transaction card</h1>
                 <p  className="text-gray-600 text-sm mt-1"> Transaction Type : {type} </p>
