f6032d66ebdd41975844c69803b01ae58c7aac92
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-06-11 02:37:22 +0100

    learning nav

---
+++ week2/myReact-journey/src/pages/Dashboard.jsx
@@ -0,0 +1,8 @@
+const Dashboard = () => {
+    return (
+        <div className=" h-full w-full bg-mist-700">
+            <h1 className="text-white  text-center">Dashboard displays here </h1>
+        </div>
+    )
+}
+export default Dashboard
