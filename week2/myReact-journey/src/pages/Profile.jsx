3d4e4d4532f10c814390083d235da70098e16eab
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-06-11 02:38:52 +0100

    learning nav

---
+++ week2/myReact-journey/src/pages/Profile.jsx
@@ -0,0 +1,8 @@
+const Profile = () => {
+    return (
+                <div className=" h-full w-full bg-mist-700">
+            <h1 className="text-white text-center">Profile displays here </h1>
+        </div>
+    )
+}
+export default Profile
